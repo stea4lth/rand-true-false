5e9385e831306d34edb148c3c37ff0dc676fd721
Chris <chrisnowlan321@gmail.com>
2015-09-28 10:37:46 -0400

    Create randTruFal.js

---
+++ randTruFal.js
@@ -0,0 +1,7 @@
+/**
+	 * [randTruFal description]
+	 * @return {bool} Return true or false randomly
+	 */
+	function randTruFal(){
+		return !!Math.floor(Math.random() * 2);	// random true/false			
+	}
